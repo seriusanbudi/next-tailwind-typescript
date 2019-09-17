9ad165086104d94824ee183c888066344361fb3d
budiharta <yuu@budiharta.local>
2019-09-17 10:44:39 +0800

    :sparkls: add tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import '../styles/index.css';
+
+const Home = () => (
+  <div className='flex text-center justify-center items-center h-screen'>
+    let's make something cool
+  </div>
+);
+
+export default Home;
